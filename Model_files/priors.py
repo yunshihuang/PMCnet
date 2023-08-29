f2847e82b2e579f32410ec7cedd0e6e9ea5ececc
Yunshi HUANG <52208587+yunshihuang@users.noreply.github.com>
2023-08-29 22:05:33 +0200

    Create priors.py

---
+++ Model_files/priors.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class isotropic_gauss_prior(object):
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+
+        self.cte_term = -(0.5) * np.log(2 * np.pi)
+        self.det_sig_term = -np.log(self.sigma)
+
+    def loglike(self, x, do_sum=True):
+
+        dist_term = -(0.5) * ((x - self.mu) / self.sigma) ** 2
+        if do_sum:
+            return -(self.cte_term + self.det_sig_term + dist_term).sum()
+        else:
+            return -(self.cte_term + self.det_sig_term + dist_term) # remember the negative sign!
+        
+class laplace_prior(object):
+    def __init__(self, mu, b):
+        self.mu = mu
+        self.b = b
+
+    def loglike(self, x, do_sum=True):
+        if do_sum:
+            return (-np.log(2 * self.b) - torch.abs(x - self.mu) / self.b).sum()
+        else:
+            return (-np.log(2 * self.b) - torch.abs(x - self.mu) / self.b)
+        
